144165f9c446d0ee2602d8be4da4b89555d2e7b0
Sujith-Maharaj <sujith.maharaj02@gmail.com>
2023-04-01 00:24:14 +0530

    First comit

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+from flask_mongoengine import MongoEngine
+
+app = Flask(__name__)
+
+@app.route("/")
+def sample():
+    return "Hello Get Request..!!"
+
+@app.route("/testpost", methods=["POST"])
+def testpost():
+    if request.method == "POST":
+        data = request.json
+        return {"status": "success", "message":"Data Received"}, 200
+    else:
+        return {"status": "error", "message": "Invalid request method..!"}
+
+if __name__ =="__main__":
+    app.run(debug=True)
+
